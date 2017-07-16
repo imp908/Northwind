0a65b7d50c8a3295174783bcaececd67333ae476
imp908 <imp908@yandex.ru>
2017-07-17 00:15:52 +0400

    initial commit

---
+++ Northwind/Northwind/Controllers/ValidationController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using System.Threading.Tasks;
+
+using Northwind.Models.Validation;
+
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+
+using Northwind.DAL;
+
+namespace Northwind.Controllers
+{
+    public class ValidationController : Controller
+    {
+
+        private ApplicationUserManager UserManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+        }
+
+        // GET: Validation
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            NorthwindModel db = new NorthwindModel();
+            int a0 = (from s in db.Employees select s).Count();
+            int a = (from s in UserManager.Users select s).Count();
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(Login login_)
+        {
+            if(ModelState.IsValid)
+            {
+                ApplicationUser user = await UserManager.FindAsync(login_.Email, login_.Password);
+                if(user!=null)
+                {
+                    return  RedirectToAction("Contacts" , "Home", login_);
+                }
+                else
+                {
+                    login_.Message = @"Wrong password or username";
+                    return RedirectToAction("Register", "Validation", login_);
+                }
+            }
+            return View(login_);
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+      
+        public async Task<ActionResult> Register(Register login_)
+        {
+            if (ModelState.IsValid) 
+            {
+                ApplicationUser user = new ApplicationUser() { UserName = login_.Email  };
+                IdentityResult result = await UserManager.CreateAsync(user, login_.Password);
+
+                return RedirectToAction("Login", "Validation");
+            }
+
+            return View( );
+        }
+    }
+}
---
+++ Northwind/Northwind/Models/Validation/Validation.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Data.Entity;
+
+using System.ComponentModel.DataAnnotations;
+
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+
+using Northwind.DAL;
+
+namespace Northwind.Models.Validation
+{
+
+    public class ApplicationUser : IdentityUser
+    {
+        public int Year { get; set; }
+        public ApplicationUser()
+        {
+        }
+    }
+
+    public class ApplicationUserManager : UserManager<ApplicationUser>
+    {
+        public ApplicationUserManager(IUserStore<ApplicationUser> store)
+                : base(store)
+        {
+        }
+        public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options,
+                                                IOwinContext context)
+        {
+            NorthwindModel db = context.Get<NorthwindModel>();
+            ApplicationUserManager manager = new ApplicationUserManager(new UserStore<ApplicationUser>(db));
+            return manager;
+        }
+    }
+        
+    public class Register
+    {
+        public virtual Employees employee { get; set; }
+
+        [Key]
+        public int ID { get; set; }
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        [DataType(DataType.Password)]
+        public string PasswordConfirm { get; set; }
+    }
+
+    public class Login
+    {
+        public Login()
+        {
+            this.Message = null;
+        }
+
+        [Key]
+        public int ID { get; set; }
+
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string Message { get; set; }
+    }
+
+}
