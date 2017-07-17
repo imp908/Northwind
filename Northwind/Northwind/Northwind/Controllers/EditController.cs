ac072dc0e7b4c4b8f64fd32e9ed58b4393281a43
imp908 <imp908@yandex.ru>
2017-07-18 01:41:22 +0400

    registration + auth

---
+++ Northwind/Northwind/Northwind/Controllers/EditController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Northwind.Controllers
+{
+    [Authorize]
+    public class EditController : Controller
+    {
+        // GET: Employee
+        public ActionResult Orders()
+        {
+            return View();
+        }
+    }
+}
